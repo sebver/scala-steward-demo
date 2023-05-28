a02adc5af9ffff72f150833d941abc360675f466
Scala Steward <scala_steward@virtuslab.com>
2023-05-28 03:26:10 +0000

    Update scalactic to 3.2.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalaTestVersion = "3.0.9"
+  val scalaTestVersion = "3.2.16"
 
   val scalaTest = Seq(
     "org.scalactic" %% "scalactic" % scalaTestVersion,
